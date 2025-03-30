7886774380c8d04a4427db0f24e47c1b33444ff7
gucooing <90658478+gucooing@users.noreply.github.com>
2025-03-30 20:24:59 +0800

    fix .

--- baps.go
+++ baps.go
@@ -126,7 +126,7 @@ func newGin(appNet *config.HttpNet) (*gin.Engine, *http.Server) {
 	}
 	router.Use(gin.Recovery())
 	router.Use(check.Cors())
-	addr := fmt.Sprintf("%s:%s", appNet.InnerPort, appNet.InnerPort)
+	addr := fmt.Sprintf("%s:%s", appNet.InnerIp, appNet.InnerPort)
 	if appNet.Tls {
 		logger.Info("监听地址: https://%s", addr)
 		logger.Info("对外地址: %s", config.GetHttpNet().GetOuterAddr())
